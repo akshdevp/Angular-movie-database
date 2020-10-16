9b9125be682a3b5a360744a047677616b155a0cd
akshith reddy <akshdevp@gmail.com>
2020-10-16 09:10:10 -0500

    a change in dataservice .ts

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -122,7 +122,7 @@ export class DataService {
       img_uri: 'assets/movies/qk.jpg',
       description: "When bitten by a genetically modified spider, a nerdy, shy, and awkward high school student gains spider-like abilities",
       comments: [],
-      rating: 0,
+      rating: 2,
     },
   ];
 
@@ -224,3 +224,4 @@ export class DataService {
     return this.searchValue;
   }
 }
+
